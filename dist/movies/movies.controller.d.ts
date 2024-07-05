3b45e571fd635f2132f86cb621e599af66e56a1d
kons2003 <mj030313@naver.com>
2024-07-06 05:40:04 +0900

    NestJS로 API 만들기 0~2 공부

---
+++ dist/movies/movies.controller.d.ts
@@ -0,0 +1,7 @@
+export declare class MoviesController {
+    getAll(): string;
+    getOne(movieId: string): string;
+    create(movieData: any): any;
+    remove(movieId: string): string;
+    patch(movieId: string, updateData: any): any;
+}
